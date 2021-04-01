391309c1e06afbee75269db9ea00269da476b5e6
shrik <shrik@DESKTOP-C116S1M>
2021-04-01 00:42:29 -0400

    passing parameters

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_restful import Api, Resource
-
+import collections
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data":"Hello World"}
+names = collections.defaultdict(lambda:{})
+names["shrikanth"] = {"age":25, "gender":"M", "status":"active"},
+names["Barry"] = {"age":99, "gender":"M", "status":"not active"}
 
-api.add_resource(HelloWorld, "/helloworld")
+class HelloWorld(Resource):
+    def get(self, name):
+        return names[name]
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if __name__ == "__main__":
     app.run(debug=True)
