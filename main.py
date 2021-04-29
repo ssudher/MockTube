fffec38384e228953f5901fc48168ad57f20b15d
shrik <shrik@DESKTOP-C116S1M>
2021-04-29 14:45:48 -0400

    Added features

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 import collections
 
 app = Flask(__name__)
 api = Api(app)
 
-names = collections.defaultdict(lambda:{})
-names["shrikanth"] = {"age":25, "gender":"M", "status":"active"},
-names["Barry"] = {"age":99, "gender":"M", "status":"not active"}
+videos = {}
+generic_response = {"Status" : "All OK"}
+class Video(Resource):
+    def get(self, video_id):
+        if video_id in videos:
+            return videos[video_id]
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
+    def put(self, video_id):
+        if video_id in videos:
+            return generic_response
+        else:
+            videos[video_id] = "newVideo"
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE+"helloworld/Barry")
+response = requests.put(BASE+"/video/123")
+
+#response = requests.get(BASE+"/video/123")
+
 print(response.json())
